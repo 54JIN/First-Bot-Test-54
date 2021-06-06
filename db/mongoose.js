ccc773f8be4b4c521e5528ae831e99ce73758d5e
Sajin Saju <sajinsaju2002@gmail.com>
2021-06-06 18:03:16 -0400

    mongodb bug fix trial 3

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,5 +4,6 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URl, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
 })
